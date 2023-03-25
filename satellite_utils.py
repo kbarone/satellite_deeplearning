4dc9519e0eccc3c00e2f21207b776936bf527709
kbarone <kbarone@uchicago.edu>
2023-03-25 17:43:52 -0500

    added run function

--- satellite_utils.py
+++ satellite_utils.py
@@ -2,6 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+import os
+import cv2
+from PIL import Image
+import numpy as np
+from patchify import patchify
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from matplotlib import pyplot as plt
+import random
+
 def hex_to_rgb(class_type):
     """
     Convert hex color value to RGB
@@ -54,3 +63,81 @@ def to_categorical(labels, num_classes):
         Array of "num_classes"-dimensional mask labels
     '''
     return np.eye(num_classes, dtype='uint8')[labels]
+
+
+!pip install patchify
+!pip install segmentation_models_pytorch
+
+minmaxscaler = MinMaxScaler()
+dataset_root_folder = '/content/drive/MyDrive/satellite'
+image_patch_size = 256
+
+def run():
+    '''
+    Read in and process satellite images
+    Inputs: None
+    Returns
+        image_dataset: (array) array of 3D images
+        labels: (array) array of 2D mask labels
+    '''
+    image_dataset = []
+    mask_dataset = []
+
+    for typ in ['images', 'masks']:
+    if typ == 'images':
+        ext = 'jpg'
+    else:
+        ext = 'png'
+    for tile_id in range(1,8):
+        for image_id in range(1,20):
+        image = cv2.imread(f'{dataset_root_folder}/Tile {tile_id}/{typ}/image_part_00{image_id}.{ext}', 1)
+        if image is not None:
+            if typ == 'masks':
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            size_x = (image.shape[1]//image_patch_size)*image_patch_size
+            size_y = (image.shape[0]//image_patch_size)*image_patch_size
+            image = Image.fromarray(image)
+            image = image.crop((0,0,size_x,size_y))
+            image = np.array(image)
+            image_patches = patchify(image, (image_patch_size, image_patch_size, 3), 
+                                        step=image_patch_size)
+            for i in range(image_patches.shape[0]):
+            for j in range(image_patches.shape[1]):
+                if typ == 'images':
+                ind_patched_img = image_patches[i,j,:,:]
+                ind_patched_img = minmaxscaler.fit_transform(ind_patched_img.reshape(-1, 
+                                                            ind_patched_img.shape[-1])).reshape(
+                                                                ind_patched_img.shape)
+                ind_patched_img = np.squeeze(ind_patched_img)
+                image_dataset.append(ind_patched_img)
+                else:
+                ind_patched_mask = image_patches[i,j,:,:]
+                ind_patched_mask = np.squeeze(ind_patched_mask)
+                mask_dataset.append(ind_patched_mask)
+
+    image_dataset = np.array(image_dataset)
+    mask_dataset = np.array(mask_dataset)
+
+    # plot a random image and its corresponding label
+    plot_random_image(image_dataset, mask_dataset)
+
+    # create dictionary of RGB values for each class
+    class_dict = {'building':'#3C1098', 'land':'#8429F6', 
+                'road': '#6EC1E4', 'vegetation':'#FEDD3A', 
+                'water':'#E2A929', 'unlabeled':'#9B9B9B'}
+    class_dict_rgb = {}
+    for key, val in class_dict.items():
+    class_dict_rgb[key] = class_dict_rgb.get(key, hex_to_rgb(val))
+
+    labels = []
+    for i in range(mask_dataset.shape[0]):
+    label = rgb_to_label(mask_dataset[i])
+    labels.append(label)
+
+    labels = np.expand_dims(labels, axis=3)
+
+    plot_random_image(image_dataset, labels)
+
+    return image_dataset, labels
+
+
